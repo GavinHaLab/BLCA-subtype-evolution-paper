1bd9d29d7ce7459904a957c4ab1ed7c98a46bcfa
Pushpa Itagi <88563212+pushpa-itagi@users.noreply.github.com>
2025-06-30 15:34:47 -0700

    Update muse_file.sh

--- pipelines/muse_file.sh
+++ pipelines/muse_file.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Basch script to launch a job with one tumor and normal pair bam file for running MuSE caller. This script was used in a wrapper for running multiple samples on the HPC cluster. 
+# Bash script to launch a job with one tumor and normal pair bam file for running MuSE caller. This script was used in a wrapper for running multiple samples on the HPC cluster. 
 #SBATCH -N1 -n4 -t 3-0
 
 MAINDIR=/path/to/Muse/Directory
